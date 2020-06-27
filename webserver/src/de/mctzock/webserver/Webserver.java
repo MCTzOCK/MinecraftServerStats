b64b627637663a1e5f7217b8eb05a723cec3b7d9
MCTzOCK <53553315+MCTzOCK@users.noreply.github.com>
2020-06-27 15:42:33 +0200

    Add files via upload

---
+++ webserver/src/de/mctzock/webserver/Webserver.java
@@ -0,0 +1,90 @@
+package de.mctzock.webserver;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Webserver {
+	
+	public boolean online = false;	
+	public ArrayList<Socket> clients = new ArrayList<Socket>();
+	public ServerSocket server = null;
+	public Integer port = 80;
+	public String response = "";
+	
+	public Webserver(Integer port) throws Exception
+	{
+		this.port = port;
+	}
+	
+	public void start() throws Exception
+	{
+		System.out.println("[WEBSERVER] Setting serverstatus to ONLINE!");
+		server = new ServerSocket(this.port);
+		this.online = true;
+		while(this.isOnline())
+		{
+			this.handlePackets();
+		}
+	}
+	
+	public void stop() throws Exception
+	{
+		System.out.println("[WEBSERVER] Setting serverstatus to OFFLINE!");
+		server.close();
+		this.online = false;
+	}
+	
+	public boolean isOnline()
+	{
+		return this.online;
+	}
+	
+	public void handlePackets() throws Exception
+	{
+		Socket client = server.accept();
+		clients.add(client);
+		
+		PrintWriter w = new PrintWriter(client.getOutputStream());
+		BufferedOutputStream dataOut = new BufferedOutputStream(client.getOutputStream());
+		byte[] data = this.toByteArray(this.response);
+		
+		w.println("HTTP/1.1 200 OK");
+		w.println("Server: Java HTTP Webserver from MCTzOCK : 1.0");
+		w.println("Date: " + new Date());
+		w.println("Content-type: text/html");
+		w.println("Content-lenght: " + this.response.length());
+		w.println();
+		w.flush();
+		
+		dataOut.write(data);
+		dataOut.flush();
+		
+		w.close();
+		dataOut.close();
+		
+		client.close();
+		clients.remove(client);
+	}
+	
+	public byte[] toByteArray(String n) throws IOException 
+	{
+		byte[] fileData = n.getBytes();
+		return fileData;
+	}
+	
+	public void setResponse(String html)
+	{
+		System.out.println("[WEBSERVER] Setting response");
+		this.response = html;
+	}
+	
+	public String getResponse()
+	{
+		return this.response;
+	}
+}
